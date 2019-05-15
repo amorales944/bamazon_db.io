1aa450874af6201812d299b514d5f03ecfd4d1bb
Armando Morales <amorales944@gmail.com>
2019-05-15 04:40:46 -0500

    Created the logic for the inquirer.prompt section the 2 questions are asked and the responses are captured, now I need to take the answers and query bamazon_db

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,10 +16,56 @@ connection.connect(function (err) {
 });
 
 function runApp(){
-connection.query("SELECT * FROM bamazon_db.products" + "\n", function(err, res) {
-  
+connection.query("SELECT * FROM bamazon_db.products", function(err, res) {
     console.log(res);
-  
-  // runSearch();
+    console.log("==============================================");
+
+  firstQuestion();
 });
 }
+
+
+function firstQuestion() {
+inquirer
+  .prompt([
+{
+  type: "input",
+  name: "item_id",
+  message: "Please input the ID of the product you would like to buy."
+},
+{
+  type: "input",
+  name: "quantity",
+  message: "How many units of this product you would like to buy."
+}
+
+])
+.then(function(user){
+// if(user.name === ){
+  console.log("==============================================");
+  console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+  console.log("==============================================");
+
+// }
+
+
+})
+}
+
+
+// function mainSearch(){
+// inquirer.prompt({
+//     type: "input",
+//     item_id: "id",
+//     message: "Please input the ID of the product they would like to buy."
+//   }),
+//   .then(function(answer)){
+//     // const query = "SELECT";
+//     connection.query("SELECT item_id from bamazon_db.products;", function(err, res) {
+//       console.log(res);
+    
+//     // runSearch();
+//   });
+
+//   }
+// }
