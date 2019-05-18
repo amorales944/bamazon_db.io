a03eb6d0f230166b60bb35f86329d5650dd6abf9
Armando Morales <amorales944@gmail.com>
2019-05-18 01:17:33 -0500

    Changed cliAPP.png picture

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,7 +19,7 @@ function runApp(){
 connection.query("SELECT * FROM bamazon_db.products", function(err, res) {
     console.log(res);
     console.log("==============================================");
-
+    
   firstQuestion();
 });
 }
@@ -39,54 +39,146 @@ inquirer
   message: "How many units of this product you would like to buy."
 }
 
-])
-.then(function(user){
-//   switch (user.action) {
-//     case "1":
-//       console.log("==============================================");
+  ])
+    .then(function(user){
+      switch (user.item_id) {
+        case "1":
+            connection.query(`SELECT * FROM products WHERE item_id = 1`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+          break;
 
-      console.log("==============================================");
-      console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
-      console.log("==============================================");
-    //   break;
+        case "2":
+          connection.query(`SELECT * FROM products WHERE item_id = 2`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+          break;
 
-    // case "2":
-    //   multiSearch();
-    //   break;
+        case "3":
+          connection.query(`SELECT * FROM products WHERE item_id = 3`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+          break;
 
-    // case "3":
-    //   rangeSearch();
-    //   break;
+        case "4":
+          connection.query(`SELECT * FROM products WHERE item_id = 4`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+          break;
 
-    // case "4":
-    //   songSearch();
-    //   break;
+        case "5":
+          connection.query(`SELECT * FROM products WHERE item_id = 5`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+          break;
 
-    // case "5":
-    //   songAndAlbumSearch();
-    //   break;
+        case "6":
+            connection.query(`SELECT * FROM products WHERE item_id = 6`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+            break;
 
-    //   case "6":
-    //     artistSearch();
-    //     break;
+        case "7":
+            connection.query(`SELECT * FROM products WHERE item_id = 7`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+            break;
 
-    //   case "7":
-    //     multiSearch();
-    //     break;
+        case "8":
+            connection.query(`SELECT * FROM products WHERE item_id = 8`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+            break;
 
-    //   case "8":
-    //     rangeSearch();
-    //     break;
+        case "9":
+            connection.query(`SELECT * FROM products WHERE item_id = 9`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+            break;
 
-    //   case "9":
-    //     songSearch();
-    //     break;
-
-    //   case "10":
-    //     songAndAlbumSearch();
-    //     break;
-    // }
-
-
-})
+        case "10":
+            connection.query(`SELECT * FROM products WHERE item_id = 10`, function (err, res) {
+              if (err) throw err;
+                console.log("==============================================");
+                console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+                console.log("==============================================");
+                console.log(res);
+              });
+            break;
+      }
+    });
 }
+
+
+// function idSearch(item_id, callback) {
+//   connection.query(`SELECT * FROM products WHERE item_id = ${item_id}`, function (err, res) {
+//       if (err) throw err;
+//         console.log("==============================================");
+//         console.log("You picked ITEM ID- " + user.item_id + " QUANTITY- " + user.quantity)
+//         console.log("==============================================");
+//       callback(res[0]);
+//   });
+// }
+
+
+
+
+
+// connection.connect(function (err) {
+//   if (err) throw err;
+//   console.log("connected as id " + connection.threadId);
+//   connection.query("SELECT * FROM songs WHERE artist = 'Daft Punk'", function (err, res) {
+//       if (err) throw err;
+//       console.log(res);
+//   });
+//   connection.query("SELECT * FROM songs LIMIT 200 OFFSET 150", function (err, res) {
+//       if (err) throw err;
+//       console.log(res);
+//   });
+//   connection.query("SELECT * FROM songs WHERE song_title = 'Hound Dog'", function (err, res) {
+//       if (err) throw err;
+//       console.log(res);
+//   });
+//   connection.query('SELECT artist, COUNT(*) FROM songs GROUP BY artist HAVING COUNT(*) >1'
+//       , function (err, res) {
+//           if (err) throw err;
+//           console.log(res);
+//       });
+//   connection.end();
+// });
